0ee58a39862d5bb99118243141b71a2f426efebf
Jonah Waltz-Rieber <jonahwr@verizon.net>
2020-12-20 09:38:47 -0500

    Entry Point Episode 2 Completed

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//Used by Hazel applications
+#include "Hazel/Application.h"
+
+//--------ENTRY POINT---------------------------------
+#include "Hazel/EntryPoint.h"
+//--------END ENTRY POINT-----------------------------
--- Hazel/src/Test.cpp
+++
@@ -1,8 +0,0 @@
-#include "Test.h"
-#include <stdio.h>
-
-namespace Hazel {
-	void Print() {
-		printf("Welcome to the Hazel Engine!\n");
-	}
-}

a26d709fbf833b8baebd04a45e199ce40fa87831
Jonah Waltz-Rieber <jonahwr@verizon.net>
2020-12-20 12:41:51 -0500

    Logging Episode 3 Completed

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,7 +2,7 @@
 
 //Used by Hazel applications
 #include "Hazel/Application.h"
-
+#include "Hazel/Log.h"
 //--------ENTRY POINT---------------------------------
 #include "Hazel/EntryPoint.h"
 //--------END ENTRY POINT-----------------------------
